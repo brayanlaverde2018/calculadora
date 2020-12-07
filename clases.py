bbb62d4ee3d8afc3ab53e498ec2404371e76eb56
afcruzc <afcruzc@gmail.com>
2020-12-07 15:03:30 -0500

    segunda subida a git

--- clases.py
+++ clases.py
@@ -1,27 +1,43 @@
 class Coche():
-    largoChasis=250
-    anchoChasis=120
-    ruedas=4
-    enmarcha=False
     
-    def arrancar(self):
-        """
-        Este metodo hace arancar al coche
-        """
-        self.enmarcha = True
-
-    def estado(self):
-        if(self.enmarcha):
+    def __init__(self):
+        
+        self.__largoChasis=250
+        self.__anchoChasis=120
+        self.__ruedas=4
+        self.__enmarcha=False
+    
+    def arrancar(self,arrancamos):
+        self.__enmarcha = arrancamos
+        
+        if(self.__enmarcha):
+            chequeo=self.__chequeointerno()
+        if(self.__enmarcha and chequeo):
             return "El coche esta en marcha"
+        elif(self.__enmarcha and chequeo==False):
+            return "Hay algo mal en el chequeo, no se puede arrancar"
         else:
             return "El coche esta detenido"
+    
+    def estado(self):
+        print("El coche tiene ",self.__ruedas,"ruedas. Un ancho de", self.__anchoChasis,"Y un largo de ",self.__largoChasis)
         
+    def __chequeointerno(self):
+        print("Realizando chequeo interno")
+        self.gasolina="ok"
+        self.aceite="ok"
+        self.puertas="cerradas"
+        
+        if self.gasolina=="ok" and self.aceite=="ok" and self.puertas=="cerradas":
+            return True
+        else:
+            return False
 miCoche=Coche()
+print(miCoche.arrancar(True))
+miCoche.estado()
 
-print(miCoche.largoChasis)
-print("El coche tiene: ",miCoche.ruedas,"ruedas")
-miCoche.arrancar()
+print("-----------------A continuación esta el segundo objeto-----------------------")
 
-print(miCoche.estado())
-
-print("A continuación esta el segundo objeto")
+miCoche2=Coche()
+print(miCoche2.arrancar(False))
+miCoche2.estado()
